7d5d614db52e859c7455128f3c09ec82929dc311
grimbleton-the-dreamer <mattk6564@aol.com>
2025-02-03 18:22:37 -0500

    Change to be dev

--- thr_atomic.c
+++ thr_atomic.c
@@ -2,10 +2,74 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <semaphore.h>
 
-double gsum = 0;
+long gSum = 0;
+pthread_mutex_t lk; 
+sem_t s;
 
-//argv[1] = m, argv[2] = n
-int main(int argc, char *argv[]){
-    printf("%s %s", argv[1], argv[2]);
+int main(int argc, char**argv) {
+    if(argc != 2){
+        printf("Incorrect number of command line arguements");
+        return 1;
+    }
+    sem_init(&s,0,0); //0 or 1 as last param?
+    int m = atoi(argv[1]);
+    int n = atoi(argv[2]);
+
+    pthread_t threads[m]; 
+    for(int i = 0; i < m; i++){
+        //change this to be dependent on iteration
+        //rename this struct probly
+        sumSquareRootsArgs* parametersCurrent = {(i*(n/m)+1,(i+1)*(n/m))};
+        pthread_create(&threads[i],NULL,sumOfSqaureoots,(void *)parametersCurrent);
+        
+    }
+    //cannot use pthread join here
+
+    //semaphore sync
+    for (int i = 0; i < m; i++){
+        sem_wait(&s); //check if this is right
+    }
+
+    //What should this say? I don't think this is the place to do it
+    //move to for loop that called
+    printf("Sum: %f",gSum);
+
+    //clean up
+    sem_destroy(&s);
+    pthread_mutex_destroy(&lk);
+    return 0;
 }
+
+//don't think we need this actually
+int isPowerOf2(int num){
+    int n = 0;
+    while(pow(2,n) < num){
+        if(pow(2,n) == num){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void* sumOfSqaureoots(void* parameters){
+    sumSquareRootsArgs* params = (sumSquareRootsArgs*)(parameters);
+    long squareRootSum = 0;
+    for(int i = params->start; i < params->end; i++){
+        squareRootSum += sqrt(i);
+    }
+
+    //safety stuff to prevent race conditions
+    pthread_mutex_lock(&lk);
+    gSum = squareRootSum;
+    pthread_mutex_unlock(&lk);
+
+    //semaphore stuff
+    sem_post(&s);
+}
+
+typedef struct{
+    int start;
+    int end;
+}sumSquareRootsArgs;
