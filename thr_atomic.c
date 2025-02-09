998b2cb5c0fbb50f7cdf782eb9428fa0f9eb67a6
lain <bunnyboyhell@outlook.com>
2025-02-08 23:13:00 -0500

    finished thr_atomic.c

--- thr_atomic.c
+++ thr_atomic.c
@@ -1,74 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <string.h>
 #include <semaphore.h>
+#include <pthread.h>
+#include <math.h>
 
-long gSum = 0;
-pthread_mutex_t lk; 
+/******************************************************************************
+ * Program Name: thr_atomic
+ * Authors: Ray Feingold 2804053, Matthew Kovach
+ * Course: CIS345 Section 50
+ * Date: 02/14/2025
+ * Description: Utilize parallel processing among 'm' number of threads to calculate
+ * the sum of square roots from 1 to 'n'
+ *****************************************************************************/
+
+// initialize global square root sum, mutex, and semaphore to share among 'm' number of threads
+double gsum = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 sem_t s;
 
-int main(int argc, char**argv) {
-    //add pthread error checking
-    if(argc != 2){
-        printf("Incorrect number of command line arguements");
+/**
+ * @brief This struct is used by threads to store the range of integers a thread will
+ * caluclate the partial sum of square roots of. 
+ * @struct pthread_sqrt_t
+ * @param l_bound integer range lower bound
+ * @param u_bound integer range upper bound
+ */
+typedef struct{
+    int tid; /**< pthread id */
+    int l_bound; /**< square root sum lower bound */
+    int u_bound; /**< square root sum upper bound */
+} pthread_sqrt_t;
+
+/**
+ * @brief Computes the sum of square roots from the lower to upper bound of a pthread_sqrt_t
+ * @param thread_args The pthread_sqrt_t passed during a pthread_create call
+ */
+void *calc(void *thread_args){
+    // create local pthread_sqrt_t variable received from pthread_create args
+    pthread_sqrt_t *thread_m = (pthread_sqrt_t*)thread_args;
+
+    // initialize local thread partial square root sum
+    double lsum = 0;
+
+    // calculate sum of square roots from the upper and lower bounds of struct pthread_sqrt_t
+    for(int i = thread_m->l_bound; i <= thread_m->u_bound; i++){
+        lsum += sqrt(i);
+    }
+
+    /**
+     * to prevent race conditions during parallel thread processing,
+     * we wrap the global variable gsum with a pthread mutex
+     * this area is known as the 'critical region'
+     */
+
+    pthread_mutex_lock(&lock);
+    gsum += lsum;
+    printf("thr[%d]: %.6f\n", thread_m->tid, lsum);
+    pthread_mutex_unlock(&lock);
+
+    // clean up, perform semaphore synchronization, and exit thread
+    free(thread_m);
+    sem_post(&s);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char** argv){
+    // check if m & n are provided in command-line arguments
+    if(argc != 3){
+        printf("\nIncorrect number of command-line arguments provided\n");
         return 1;
     }
-    sem_init(&s,0,0); //0 or 1 as last param?
+
+    // initialize semaphore 
+    sem_init(&s, 0, 0);
+
+    // from command-line args-> 'm' = number of threads, 'n' = integer range upper bound
     int m = atoi(argv[1]);
     int n = atoi(argv[2]);
 
-    pthread_t threads[m]; 
+    /**
+     * create 'm' number of threads to calculate partial square root sums
+     * between the determined lower to upper bound integer range
+     * of current thread_m
+     */
+    pthread_t thr[m];
     for(int i = 0; i < m; i++){
-        //change this to be dependent on iteration
-        //rename this struct probly yes do this
-        //we need proper allocation here!
-        sumSquareRootsArgs parametersCurrent = {(i*(n/m)+1,(i+1)*(n/m))};
-        pthread_create(&threads[i],NULL,sumOfSqaureRoots,(void *)&parametersCurrent);
+        pthread_sqrt_t *thread_m = malloc(sizeof(pthread_sqrt_t));
+        thread_m->tid = i;
+        thread_m->l_bound = i * (n / m) + 1;
+        thread_m->u_bound = (i + 1) * (n / m);
         
-    }
-    //cannot use pthread join here
-
-    //semaphore sync
-    for (int i = 0; i < m; i++){
-        sem_wait(&s); //check if this is right
-        
+        // check if successful thread creation
+        int t = pthread_create(&thr[i], NULL, calc, (void*)thread_m);
+        if(t != 0){
+            printf("Error creating thread %d\n", i);
+            return 1;
+        }
     }
 
-    //What should this say? I don't think this is the place to do it
-    printf("Sum: %f",gSum);
+    // perform semaphore synchronization among threads
+    for(int i = 0; i < m; i++){
+        sem_wait(&s);
+    }
 
-    //clean up
+    printf("sum of square roots: %.6f\n", gsum);
+
+    // clean up
     sem_destroy(&s);
-    pthread_mutex_destroy(&lk);
+    pthread_mutex_destroy(&lock);
     return 0;
 }
-
-void* sumOfSqaureRoots(void* parameters){
-    sumSquareRootsArgs* params = (sumSquareRootsArgs*)(parameters);
-    long squareRootSum = 0;
-    for(int i = params->start; i < params->end; i++){
-        squareRootSum += sqrt(i);
-    }
-
-    //safety stuff to prevent race conditions
-    pthread_mutex_lock(&lk);
-    gSum = squareRootSum;
-    pthread_mutex_unlock(&lk);
-
-    //pthread_t thread_id = pthread_self();
-
-    //NEED A PRINT HERE!!!
-
-    //semaphore stuff
-    sem_post(&s);
-    //pthread_exit(NULL);
-}
-
-//this could also be an enum
-//Should params be type int?
-//We need to pass in the tid of the thread for debugging
-typedef struct{
-    int start;
-    int end;
-}sumSquareRootsArgs;
